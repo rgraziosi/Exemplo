dc430ed14dda1ee91dd121c2026139a9c876e9ce
Rafael <ti@vxm.com.br>
2018-03-17 19:21:10 -0300

    Inicio da utilização do Vue para requisição, erro de CORS na chamada, Access-Control-Allow-Origin.

--- js/all.js
+++ js/all.js
@@ -1,1 +1,19 @@
 // JavaScript Document
+var app = new Vue({
+  el: '#vue',
+  data: {
+    posts: [    ]
+  },
+	mounted() {
+		fetch("https://www.mocky.io/v2/5a6bc16631000078341b8b77", {
+			headers:{
+				'Access-Control-Allow-Origin':'*',
+				'crossDomain': 'false'
+			}
+		})
+		.then(response => response.json())
+		.then((data) => {
+			this.posts = data.links;
+		})
+	}
+});
